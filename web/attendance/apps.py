4646c5979ac5b22a8ae18772424c89764aa849b3
sangchun1 <sangchunpark18@apu.edu>
2023-07-04 15:17:33 +0900

    read two cameras

--- web/attendance/apps.py
+++ web/attendance/apps.py
@@ -11,12 +11,18 @@ class AttendanceConfig(AppConfig):
         from attendance.views import CameraBackgroundTask
         from attendance.signals import camera_task_stopped
 
-        # 서버 시작 시 카메라 작업 실행
-        camera_task = CameraBackgroundTask()
-        camera_task.start()
+        try:
+            # 서버 시작 시 카메라 작업 실행
+            camera_task1 = CameraBackgroundTask(0)
+            camera_task1.start()
+            camera_task2 = CameraBackgroundTask(1)
+            camera_task2.start()
 
-        # 서버 종료 시 카메라 작업 종료
-        def stop_camera_task(sender, **kwargs):
-            camera_task.stop()  # 작업을 종료하는 메서드 호출
+            # 서버 종료 시 카메라 작업 종료
+            def stop_camera_task(sender, **kwargs):
+                camera_task1.stop()  # 작업을 종료하는 메서드 호출
+                camera_task2.stop()
 
-        camera_task_stopped.connect(stop_camera_task)
+            camera_task_stopped.connect(stop_camera_task)
+        except cv2.error:
+            pass
